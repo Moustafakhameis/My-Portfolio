930704a9914c1b9c15b4c0e30054c8933fb85011
Mostafa Ali <moustafakhameis@gmail.com>
2026-07-03 18:10:06 +0300

    fix: scope gallery touch and mouse events to container to prevent page scroll lag

--- src/components/ui/CircularGallery.jsx
+++ src/components/ui/CircularGallery.jsx
@@ -585,13 +585,14 @@ class App {
     };
 
     window.addEventListener('resize', this.boundOnResize);
-    window.addEventListener('mousedown', this.boundOnTouchDown);
     window.addEventListener('mousemove', this.boundOnTouchMove);
     window.addEventListener('mouseup', this.boundOnTouchUp);
-    window.addEventListener('touchstart', this.boundOnTouchDown);
     window.addEventListener('touchmove', this.boundOnTouchMove);
     window.addEventListener('touchend', this.boundOnTouchUp);
 
+    this.container?.addEventListener('mousedown', this.boundOnTouchDown);
+    this.container?.addEventListener('touchstart', this.boundOnTouchDown);
+    this.container?.addEventListener('wheel', this.boundOnWheel, { passive: true });
     this.container?.addEventListener('keydown', this.boundOnKeyDown);
     this.container?.addEventListener('mouseenter', this.boundOnMouseEnter);
     this.container?.addEventListener('mouseleave', this.boundOnMouseLeave);
@@ -599,10 +600,8 @@ class App {
   destroy() {
     window.cancelAnimationFrame(this.raf);
     window.removeEventListener('resize', this.boundOnResize);
-    window.removeEventListener('mousedown', this.boundOnTouchDown);
     window.removeEventListener('mousemove', this.boundOnTouchMove);
     window.removeEventListener('mouseup', this.boundOnTouchUp);
-    window.removeEventListener('touchstart', this.boundOnTouchDown);
     window.removeEventListener('touchmove', this.boundOnTouchMove);
     window.removeEventListener('touchend', this.boundOnTouchUp);
     if (this.renderer && this.renderer.gl && this.renderer.gl.canvas.parentNode) {
@@ -610,6 +609,9 @@ class App {
     }
 
     if (this.container) {
+      this.container.removeEventListener('mousedown', this.boundOnTouchDown);
+      this.container.removeEventListener('touchstart', this.boundOnTouchDown);
+      this.container.removeEventListener('wheel', this.boundOnWheel);
       this.container.removeEventListener('keydown', this.boundOnKeyDown);
       this.container.removeEventListener('mouseenter', this.boundOnMouseEnter);
       this.container.removeEventListener('mouseleave', this.boundOnMouseLeave);
